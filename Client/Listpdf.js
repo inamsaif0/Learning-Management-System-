ce28c015aabb1cc4ad2a82570de75589883bc157
MaaZ Asim <maazasim@MaaZs-MacBook-Pro.local>
2023-06-16 13:32:14 +0500

    added open pdf function+ added check for non pdf files should download

--- Client/Listpdf.js
+++ Client/Listpdf.js
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
-import {Modal,StyleSheet, Text, View, FlatList, Image, TouchableOpacity, Pressable, Linking,ActivityIndicator } from 'react-native';
+import { Modal, StyleSheet, Text, View, FlatList, Image, TouchableOpacity, Pressable, Linking, ActivityIndicator } from 'react-native';
 import { WebView } from 'react-native-webview';
 import pdf from './assets/pdf.png';
 import { UserContext } from './App';
-const PdfReader = ({ url: uri }) => <WebView javaScriptEnabled={true}  style={{ flex: 1 }} source={{ uri }} />
+import { AntDesign } from '@expo/vector-icons';
 export default function Listpdf() {
   const email = React.useContext(UserContext);
 
   const [docs, setDocs] = useState(null);
   const [selectedItem, setSelectedItem] = useState(false);
-  const [isLoading,setIsloading]=useState(true)
+  const [notPdf,setNonPdf]=useState(false);
+  const [isLoading, setIsloading] = useState(true)
 
   React.useEffect(() => {
     async function getDocs(email) {
       try {
         console.log(email)
         const response = await fetch(`http://192.168.1.4:3000/documents?email=${email}`, { method: 'GET' })
-        .then((response)=>response.json())
-        .then((data)=>setDocs(data))
-        .then(()=>setIsloading(false))
-        .catch((error) => console.error('Error retrieving doc files:', error)); 
-        
+          .then((response) => response.json())
+          .then((data) => setDocs(data))
+          .then(() => setIsloading(false))
+          .catch((error) => console.error('Error retrieving doc files:', error));
+
       } catch (error) {
         console.log("Error getting documents", error);
       }
@@ -31,47 +32,53 @@ export default function Listpdf() {
   function openWebView(url) {
     setSelectedItem(url);
   }
-  
 
-  
+
+
 
   const [modalVisible, setModalVisible] = useState(false);
   function renderListItem({ item }) {
+    if(!item.filename.split('.').pop()==="pdf")
+    {
+      setNonPdf(true)
+    }
 
     return (
       <View>
         <Modal animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-          <View>
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
 
-          <PdfReader url={item.fileUrl} />
-          </View>
+          <Pressable style={{ padding: 20,alignItems:"flex-end" }} onPress={() => setModalVisible(false)}>
+            <AntDesign name="closecircle" size={24} color="black" />
+          </Pressable>
 
+
+          <WebView javaScriptEnabled={true} style={{ flex: 1 }} source={{ uri: item.fileUrl }} />
         </Modal>
-        
-      <Pressable onPress={() =>setModalVisible(true)} style={styles.listItem}>
-        <Image source={pdf} style={{ width: 60, height: 60, borderRadius: 30 }} />
-        <View style={{ alignItems: "center", flex: 1 }}>
-          <Text style={{ fontWeight: "bold" }}>{item.filename}</Text>
-          <Text>{item.position}</Text>
-        </View>
-      </Pressable>
+
+        <Pressable onPress={() => !notPdf?setModalVisible(true):Linking.openURL(item.fileUrl)} style={styles.listItem}>
+          <Image source={pdf} style={{ width: 60, height: 60, borderRadius: 30 }} />
+          <View style={{ alignItems: "center", flex: 1 }}>
+            <Text style={{ fontWeight: "bold" }}>{item.filename}</Text>
+            <Text>{item.position}</Text>
+          </View>
+        </Pressable>
       </View>
     );
   }
 
-  if(isLoading){
-    return(
+  if (isLoading) {
+    return (
 
-      <View style={{flex:1,justifyContent:'center'}}>
-      <ActivityIndicator size={'large'} color={'#800000'}/>
-    </View>
-      )
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <ActivityIndicator size={'large'} color={'#800000'} />
+      </View>
+    )
   }
 
   return (
